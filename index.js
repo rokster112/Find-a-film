770320050fdcd639cca44b5094ac1b115714e7c5
rokas <rokster112@gmail.com>
2025-04-03 16:32:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+"use strict"
+
+const main = document.querySelector(".main")
+const searchInput = document.querySelector(".nav-input")
+const submitButton = document.querySelector(".nav-search__btn")
+let searchRes = []
+let watchlist = []
+
+async function handleChange(e) {
+    e.preventDefault()
+    searchRes = []
+    let value = searchInput.value
+    console.log(value)
+    const res = await fetch(`http://www.omdbapi.com/?&apikey=e5c482bc&s=${value}`)
+    const data = await res.json()
+    if (data.Response === "True") {
+    for (const item of data.Search) {
+      const res2 = await fetch(`http://www.omdbapi.com/?&apikey=e5c482bc&i=${item.imdbID}`)
+      const data2 = await res2.json()
+      searchRes.push(data2)
+    }
+    main.replaceChildren()
+
+      for (const item of searchRes) {
+          main.insertAdjacentHTML('beforeend', `<div class="main-card">
+          <div class="card-image-container"><img class="card-image" src=${item.Poster} alt=${item.Title}></div>
+          <div class="card-content__container">
+            <div class="card-content__heading">
+              <h3 class="card-title">${item.Title}</h3>
+              <p class="card-rating">⭐ ${item.imdbRating}</p>
+            </div>
+            <div class="card-content__main">
+            <p class="card-duration">${item.Runtime}</p>
+            <p class="card-genre">${item.Genre}</p>
+            <div class="card-button__container">
+            <button class="card-button" id=${item.imdbID}>+</button>
+            <p>Watchlist</p>
+            </div>
+            </div>
+            <p class="description">${item.Plot}</p>
+          </div>
+        </div><hr>`)
+        document.getElementById(item.imdbID).addEventListener("click", addMovie)
+      } 
+    } else {
+      main.innerHTML = '<p style="font-size: 18px; color: #DFDDDD; position: absolute; top: 30vh;">Unable to find what you’re looking for. Please try another search.</p>';
+    }
+    value = ""
+  } 
+
+  submitButton.addEventListener("click", handleChange)
+
+  async function addMovie(e) {
+    const res = await fetch(`http://www.omdbapi.com/?&apikey=e5c482bc&i=${e.target.id}`)
+    const data = await res.json()
+    console.log(data)
+    localStorage.setItem(e.target.id, JSON.stringify(data))
+    document.querySelector(`#${e.target.id}`).innerHTML = '✓'
+  }
+
+  document.querySelector(".nav-watchlist").addEventListener("click", function(event) {
+    console.log("Link clicked from home!");
+});
---
+++ watchlist.js
@@ -0,0 +1,47 @@
+
+const main = document.querySelector(".watchlist-main")
+const homeBtn = document.querySelector(".nav-watchlist")
+
+  async function getMovies() {
+    main.replaceChildren()
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i)
+      console.log(key)
+      if (key !== "backstopjs" && key) {
+        const res = await fetch(`http://www.omdbapi.com/?&apikey=e5c482bc&i=${key}`)
+        const data = await res.json()
+        main.insertAdjacentHTML('beforeend', `<div class="main-card">
+        <div class="card-image-container"><img class="card-image" src=${data.Poster} alt=${data.Title}></div>
+        <div class="card-content__container">
+        <div class="card-content__heading">
+          <h3 class="card-title">${data.Title}</h3>
+          <p class="card-rating">⭐ ${data.imdbRating}</p>
+        </div>
+        <div class="card-content__main">
+          <p class="card-duration">${data.Runtime}</p>
+          <p class="card-genre">${data.Genre}</p>
+          <div class="card-button__container">
+            <button class="card-button" id=watchlist-${data.imdbID}>-</button>
+            <p>Remove</p>
+          </div>
+          </div>
+          <p class="description">${data.Plot}</p>
+        </div>
+      </div><hr>`)
+        document.getElementById(`watchlist-${data.imdbID}`).addEventListener("click", () => {
+          localStorage.removeItem(key)
+          location.reload()
+        })
+        console.log(data)
+      } else if (localStorage.length < 2 && !key.startsWith("tt")) {
+        main.innerHTML = `<p style="color: #DFDDDD; font-size: 18px;">Your watchlist is looking a little empty...</p>
+                          <div class="watchlist-noContent">
+                          <a class="card-button" id="home-link" href="index.html">+</a><p style="color: black; font-size: 14px; font-weight: 700;">Let’s add some movies!</p>
+                          </div>`
+        homeBtn.addEventListener("click", () => {})
+      }
+
+      } 
+    }
+
+    getMovies()
