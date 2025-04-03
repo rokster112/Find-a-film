d3901cb0486e95f1749f8c27bab6001ea53a7829
rokas <rokster112@gmail.com>
2025-04-03 18:20:25 +0300

    It still does not work, I have few ideas on how to fix it. It works locally but not when deployed

--- watchlist.js
+++ watchlist.js
@@ -3,6 +3,7 @@ const main = document.querySelector(".watchlist-main")
 const homeBtn = document.querySelector(".nav-watchlist")
 
   async function getMovies() {
+    console.log(!localStorage)
     main.replaceChildren()
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i)
@@ -31,7 +32,7 @@ const homeBtn = document.querySelector(".nav-watchlist")
           localStorage.removeItem(key)
           location.reload()
         })
-      } else if ((localStorage.length < 2 && !key.startsWith("tt")) || !key) {
+      } else if ((localStorage.length < 2 && !key.startsWith("tt")) || !localStorage) {
         main.innerHTML = `<p style="color: #DFDDDD; font-size: 18px;">Your watchlist is looking a little empty...</p>
                           <div class="watchlist-noContent">
                           <a class="card-button" id="home-link" href="index.html">+</a><p style="color: black; font-size: 14px; font-weight: 700;">Let’s add some movies!</p>
